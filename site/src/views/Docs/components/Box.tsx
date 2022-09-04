120cd0a0c68c617d5da2f316fc12dc580774b9e5
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2022-09-04 12:45:45 -0500

    site\feat: docs Box attr label and gitignore .log

--- site/src/views/Docs/components/Box.tsx
+++ site/src/views/Docs/components/Box.tsx
@@ -4,17 +4,20 @@ import React from "react";
 interface IBoxProps {
   children?: ReactNode;
   className: string;
+  label?: string;
 }
 
 const Box: FC<IBoxProps> = ({
   children,
   className,
+  label,
 }: Readonly<IBoxProps>): JSX.Element => (
-  <div className={"py1"}>
-    <div className={`bg-light4 d-ib rnd`}>
+  <div className={"ai-center d-f py1"}>
+    <p className={"d-ib pr2"}>{label}</p>
+    <div className={"bg-light4 d-ib rnd"}>
       <div
         className={
-          `bdr2 bdr-dark7 bg-light4 d-ib dark4 rnd ${className}`.trimEnd()
+          `bdr2 bdr-dark7 bg-light4 d-b dark4 rnd ${className}`.trimEnd()
         }
       >
         {children}
