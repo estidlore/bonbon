a719bfa6bffec5ee8c79e52638deb03f510e88af
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2022-09-08 00:35:38 -0500

    site\feat: notation subsection

---
+++ site/src/views/Docs/components/Section/Notation.tsx
@@ -0,0 +1,60 @@
+import type { FC } from "react";
+import React, { Fragment } from "react";
+import { SubSection } from "./SubSection";
+
+interface IModifiersProps {
+  modifiers?: Record<string, string> | string[];
+  title: string;
+}
+
+interface INotationProps {
+  base: string;
+  numbers?: string[];
+  values?: Record<string, string>;
+}
+
+const Modifiers: FC<IModifiersProps> = ({
+  modifiers,
+  title
+}: Readonly<IModifiersProps>): JSX.Element | null =>
+  modifiers === undefined ? null : (
+    <Fragment>
+      <p className={"f3 mb1 mt2"}>{title}</p>
+      <div className={"d-f"}>
+        <div className={"mr2"}>
+          {Object.keys(modifiers).map((key) => (
+            <p key={`nk${key}`}>{key}</p>
+          ))}
+        </div>
+        <div className={"grow"}>
+          {Object.values(modifiers).map((val) => (
+            <p key={`nv${val}`}>{`= ${val}`}</p>
+          ))}
+        </div>
+      </div>
+    </Fragment>
+  );
+
+const Notation: FC<INotationProps> = ({
+  base,
+  numbers,
+  values,
+}: Readonly<INotationProps>): JSX.Element => (
+  <SubSection title={"Notation"}>
+    <p className={"f3 mb1 mt2"}>{"Structure"}</p>
+    <p>
+      {`${
+        base
+      }${
+        values === undefined ? "" : "-<value>"
+      }${
+        numbers === undefined ? "" : "<number>"
+      }`}
+    </p>
+    <Modifiers modifiers={values} title={"Values"} />
+    <Modifiers modifiers={numbers} title={"Numbers"} />
+  </SubSection>
+);
+
+export type { INotationProps };
+export { Notation };
--- site/src/views/Docs/components/Section/index.tsx
+++ site/src/views/Docs/components/Section/index.tsx
@@ -1,22 +1,29 @@
 import type { FC, ReactNode } from "react";
 import React from "react";
 
+import type { INotationProps } from "./Notation";
+import { Notation } from "./Notation";
 import { SubSection } from "./SubSection";
 
 interface ISectionProps {
-  examples: ReactNode;
+  examples?: ReactNode;
   id: string;
+  notation?: INotationProps;
   title: string;
 }
 
 const Section: FC<ISectionProps> = ({
   examples,
   id,
+  notation,
   title,
 }: Readonly<ISectionProps>): JSX.Element => (
   <section id={id}>
     <p className={"f6 mt5 mb3"}>{title}</p>
-    <SubSection title={"Examples"}>{examples}</SubSection>
+    {notation === undefined ? undefined : <Notation {...notation} />}
+    {examples === undefined ? undefined : (
+      <SubSection title={"Examples"}>{examples}</SubSection>
+    )}
   </section>
 );
 
