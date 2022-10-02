0e7bda9bfe05ddafcca7ddd44165bb1fba1f6dee
estidlore <estidlore@outlook.com>
2022-10-02 01:00:03 -0500

    site\feat: #2 snippet indentation

--- site/src/components/Snippet.tsx
+++ site/src/components/Snippet.tsx
@@ -16,7 +16,15 @@ const Snippet: FC<ISnippetProps> = ({
 
   return (
     <div className={"bg-light2 d-f dark4 my3 p4 rnd ws-pl"}>
-      <p className={"f3 grow"}>{code}</p>
+      <div className={"f3 grow"}>
+        {code.split("\n").map((line): JSX.Element => {
+          const spaces = line.length - line.trimStart().length;
+
+          return (
+            <p key={line} style={{ paddingLeft: `${spaces / 2}em` }}>{line}</p>
+          );
+        })}
+      </div>
       <button
         className={"as-start bdr-no bg-no hov-bg-light3 ml5 px3 py2 rnd trn2"}
         onClick={handleCopy}
